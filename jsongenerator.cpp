56966e958b708454e60a9dce84ef7f359ebbb210
Chris Barts <chbarts@gmail.com>
2016-10-20 11:00:32 -0600

    Fix JSON syntax.

--- jsongenerator.cpp
+++ jsongenerator.cpp
@@ -13,12 +13,12 @@ bool JsonGenerator::write(QIODevice *device)
     out.setDevice(device);
     out.setCodec("UTF-8");
     out << "{\n";
-    out << indent(1) << "\"folderName\": \"Bookmarks\", \"contents\": {\n";
+    out << indent(1) << "\"folderName\": \"Bookmarks\", \"contents\": [\n";
 
     for (int i = 0; i < cnt; ++i)
         generateItem(treeWidget->topLevelItem(i), 2, i == (cnt - 1));
 
-    out << indent(1) << "}\n";
+    out << indent(1) << "]\n";
     out << "}\n";
     return true;
 }
@@ -28,11 +28,10 @@ void JsonGenerator::generateItem(QTreeWidgetItem *item, int depth, bool end)
     QString tagName = item->data(0, Qt::UserRole).toString();
     if (tagName == "folder") {
         int cnt = item->childCount();
-        out << indent(depth) << "{ \"folderName\": " << escapedAttribute(item->text(0)) << ", \"contents\": {\n";
-        out << indent(depth) << "  [\n";
+        out << indent(depth) << "{ \"folderName\": " << escapedAttribute(item->text(0)) << ", \"contents\": [\n";
         for (int i = 0; i < cnt; ++i)
             generateItem(item->child(i), depth + 1, i == (cnt - 1));
-        out << indent(depth) << "  ]}\n";
+        out << indent(depth) << "  ]\n";
         out << indent(depth) << "}";
     } else if (tagName == "bookmark") {
         out << indent(depth) << "{ \"title\": " << escapedAttribute(item->text(0));
