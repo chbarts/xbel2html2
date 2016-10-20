dbb4752f357fd1b682a37bde37fdeccd257df732
Chris Barts <chbarts@gmail.com>
2016-10-20 09:55:20 -0600

    Add jsongenerator.h

---
+++ jsongenerator.h
@@ -0,0 +1,26 @@
+#ifndef JSONGENERATOR_H
+#define JSONGENERATOR_H
+
+#include <QTextStream>
+#include "generator.h"
+
+QT_BEGIN_NAMESPACE
+class QTreeWidget;
+class QTreeWidgetItem;
+QT_END_NAMESPACE
+
+class JsonGenerator : public Generator
+{
+public:
+    JsonGenerator(QTreeWidget *treeWidget);
+
+    bool write(QIODevice *device);
+
+    ~JsonGenerator();
+
+protected:
+    void generateItem(QTreeWidgetItem *item, int depth);
+};
+
+#endif // JSONGENERATOR_H
+
